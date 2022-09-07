4bb173b6d9faced41024033325eea8b0a0539a2a
abdssamad <zianiabdssamad30@gmail.com>
2022-09-07 03:49:46 -0700

    deleted files

---
+++ test un nombre.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+  void main()
+ {
+     int nb;
+     printf("Veulliez entre un nombre\n");
+     scanf("%d",&nb);
+     if(nb<0)
+        printf("ce nombre est negative\n");
+     else
+        printf("ce nombre est positif\n");
+
+
+
+
+
+
+
+
+
+
+
+
+ }
